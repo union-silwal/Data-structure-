2a69739f21b6e2c23278721ec61b8abd7b30807e
union-silwal <samyogsilwal10@gmail.com>
2024-03-14 08:34:57 +0000

    arithemetic operator c

---
+++ Arithemetic logical operator.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=14,b=12;
+	int sum=a+b;
+	int diff=a-b;
+	int pro=a*b;
+	float div=a/b;
+	int rem=a%b;
+	
+	printf("\n sum=%d",sum);
+	printf("\n difference=%d",diff);
+printf("\n product=%d",pro);
+printf("\n division=%f",div);
+printf("\n reminder=%d",rem);				
+					
+}
